9e691aaa4e7b0c1899197e53f181ca2822046226
Luis Felipe Jimenez Ferreira <xluis190@hotmail.com>
2016-10-25 16:22:38 -0400

    Add files via upload

--- Gallery/js/mainjs.js
+++ Gallery/js/mainjs.js
@@ -1,6 +1,10 @@
 $(function(){
-
 	// Initialize the gallery
 	$('.thumbs a').touchTouch();
-
 });
+for(var x = 1;x<10;x++){
+	$(".thumbs").append('<a href="img/imgpack/www.dmzwarez.info_Natures_ 000'+x+'.jpg" ><img src="img/imgpack/www.dmzwarez.info_Natures_ 000'+x+'.jpg" class="img-responsive thumbnail"></a>');
+}
+for(var x = 10;x<45;x++){
+	$(".thumbs").append('<a href="img/imgpack/www.dmzwarez.info_Natures_ 00'+x+'.jpg" ><img src="img/imgpack/www.dmzwarez.info_Natures_ 00'+x+'.jpg" class="img-responsive thumbnail"></a>');
+}
